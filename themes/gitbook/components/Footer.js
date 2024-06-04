98484698df06ffe8476c6b66199909aa27b9f3b9
Merlin Kuo <94113770+yuchen5564@users.noreply.github.com>
2024-06-04 19:47:33 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -12,25 +12,7 @@ const Footer = ({ siteInfo }) => {
       <hr className='pb-2' />
 
       <div className='flex justify-center'>
-        <div>
-          <i className='mx-1 animate-pulse fas fa-heart' />{' '}
-          <a
-            href={siteConfig('LINK')}
-            className='underline font-bold text-gray-500 dark:text-gray-300 '>
-            {siteConfig('AUTHOR')}
-          </a>
-          .<br />
-        </div>
-        © {`${copyrightDate}`}
-      </div>
-
-      <div className='text-xs font-serif'>
-        Powered By{' '}
-        <a
-          href='https://github.com/tangly1024/NotionNext'
-          className='underline text-gray-500 dark:text-gray-300'>
-          NotionNext {siteConfig('VERSION')}
-        </a>
+       台科新生懶人包 For Freshman in NTUST © {`${copyrightDate}`}
       </div>
 
       {siteConfig('BEI_AN') && (
@@ -42,15 +24,6 @@ const Footer = ({ siteInfo }) => {
           <br />
         </>
       )}
-
-      <span className='hidden busuanzi_container_site_pv'>
-        <i className='fas fa-eye' />
-        <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
-      </span>
-      <span className='pl-2 hidden busuanzi_container_site_uv'>
-        <i className='fas fa-users' />{' '}
-        <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
-      </span>
       {/* SEO title */}
       <h1 className='pt-1 hidden'>{siteConfig('TITLE')}</h1>
     </footer>
