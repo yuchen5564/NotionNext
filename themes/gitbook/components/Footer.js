c0d9cd041c41d09e5f500846f4bf909c2196bef6
yuchen5564 <94113770+yuchen5564@users.noreply.github.com>
2025-07-15 23:58:51 +0800

    Update Setting

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -15,28 +15,17 @@ const Footer = ({ siteInfo }) => {
 
   return (
     <footer className='z-20 border p-2 rounded-lg bg:white dark:border-black dark:bg-hexo-black-gray justify-center text-center w-full text-sm relative'>
+      <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "c6e712a9250f4f8785e0644a46b3159c"}'></script>
+
       <SocialButton />
 
       <div className='flex justify-center'>
-        <div>
-          <i className='mx-1 animate-pulse fas fa-heart' />{' '}
-          <a
-            href={siteConfig('LINK')}
-            className='underline font-bold text-gray-500 dark:text-gray-300 '>
-            {siteConfig('AUTHOR')}
-          </a>
-          .<br />
-        </div>
-        © {`${copyrightDate}`}
+        台科新生懶人包 For Freshman in NTUST © {`${copyrightDate}`}
       </div>
 
       {siteConfig('BEI_AN') && (
         <>
           <i className='fas fa-shield-alt' />{' '}
-          <a href={siteConfig('BEI_AN_LINK')} className='mr-2'>
-            {siteConfig('BEI_AN')}
-          </a>
-          <BeiAnGongAn />
           <br />
         </>
       )}
