c5d62c1b474b8d314afca46e3494d40926639f8b
Merlin Kuo <94113770+yuchen5564@users.noreply.github.com>
2025-07-18 22:52:34 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -23,21 +23,6 @@ const Footer = ({ siteInfo }) => {
         台科新生懶人包 For Freshman in NTUST © {`${copyrightDate}`}
       </div>
 
-      {siteConfig('BEI_AN') && (
-        <>
-          <i className='fas fa-shield-alt' />{' '}
-          <br />
-        </>
-      )}
-
-      <span className='hidden busuanzi_container_site_pv'>
-        <i className='fas fa-eye' />
-        <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
-      </span>
-      <span className='pl-2 hidden busuanzi_container_site_uv'>
-        <i className='fas fa-users' />{' '}
-        <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
-      </span>
       {/* SEO title */}
       <h1 className='pt-1 hidden'>{siteConfig('TITLE')}</h1>
     </footer>
